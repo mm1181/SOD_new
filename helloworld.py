c74b5a6877f03dabbe9438e2bf5cf31a5c8e1497
15388657851 <https://gitee.com/mt1181/experiment_-ssm_1>
2026-04-30 09:47:37 +0800

    消融实验前_备份

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,10 @@
 import os
 import sys
 
+#统计数据集数量123456
+
 def count_files_in_dirs(root_dir):
-    """递归遍历 root_dir，输出每个目录下的文件数量。"""
     for dirpath, dirnames, filenames in os.walk(root_dir):
-        # dirpath: 当前目录的绝对路径
-        # filenames: 当前目录下的直接文件列表（不包含子目录）
         print(f"{dirpath}: {len(filenames)} files")
 
 if __name__ == "__main__":
